28318ddf4ac9719465640fce21094bd0fea7a081
Dominic Webster <dominicbweiss@gmail.com>
2026-05-02 18:52:20 -0400

    enemy flash

--- scenes/temp_enemy/temp_enemy.gd
+++ scenes/temp_enemy/temp_enemy.gd
@@ -2,10 +2,14 @@ extends CharacterBody2D
 class_name TempEnemy
 
 @onready var player = get_tree().get_first_node_in_group("player")
+@onready var sprite: Sprite2D = $Sprite2D
 
 @export var move_speed : float = 100
 @export var max_health : int = 3
 var health : int = 0
+
+var is_flashing : bool = false
+
 
 func _ready():
 	health = max_health
@@ -23,6 +27,7 @@ func _physics_process(_delta: float) -> void:
 
 func take_damage(amount: int):
 	health -= amount
+	flash_red()
 	
 	if health <= 0:
 		die()
@@ -36,3 +41,14 @@ func _on_hurt_box_area_entered(area):
 	if area.is_in_group("player_bullet"):
 		take_damage(1)
 		area.queue_free()
+
+
+func flash_red():
+	if is_flashing:
+		return
+	
+	is_flashing = true
+	sprite.modulate = Color(1, 0.4, 0.4) # red
+	await get_tree().create_timer(0.1).timeout
+	sprite.modulate = Color(1, 1, 1) # back to normal
+	is_flashing = false
