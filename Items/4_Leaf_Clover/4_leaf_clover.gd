035723c23e66f288c6e7f02d2502584df3d14902
Dominic Webster <dominicbweiss@gmail.com>
2026-05-10 22:05:17 -0400

    4 leaf clover

---
+++ Items/4_Leaf_Clover/4_leaf_clover.gd
@@ -0,0 +1,13 @@
+extends Item
+class_name FourLeafClover
+
+var item_name : String = "4 Leaf Clover"
+var luck_boost : int = 1
+
+
+func _on_body_entered(_body) -> void:
+	if _body.is_in_group("player"):
+		RunManager.player.add_item_to_array(item_name)
+		RunManager.player.luck += luck_boost
+		queue_free()
+		picked_up.emit()
