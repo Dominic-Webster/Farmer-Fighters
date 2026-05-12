65b6a05dc028fb9d8947db5bbddcc5675fba3c5a
Dominic Webster <dominicbweiss@gmail.com>
2026-05-12 19:19:33 -0400

    dark soda

---
+++ Enemies/Dark_Soda/dark_soda.gd
@@ -0,0 +1,25 @@
+extends Enemy
+class_name DarkSoda
+
+
+func _physics_process(_delta: float) -> void:
+	if player == null:
+		return
+	
+	var direction = (player.global_position - global_position).normalized()
+	var move_velocity = direction * move_speed
+	
+	velocity = move_velocity + knockback_velocity
+	knockback_velocity = knockback_velocity.move_toward(Vector2.ZERO, 800 * _delta)
+	
+	move_and_slide()
+
+
+func die():
+	died.emit()
+	hurt_box.set_deferred("monitoring", false)
+	move_speed = 0
+	anim.stop()
+	anim.play("die")
+	await anim.animation_finished
+	queue_free()
