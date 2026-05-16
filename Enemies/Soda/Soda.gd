9f27dd2e3a8ad40508afa311bec3df85dbcb2a9a
Dominic Webster <dominicbweiss@gmail.com>
2026-05-16 19:34:43 -0400

    dash tweak

--- Enemies/Soda/Soda.gd
+++ Enemies/Soda/Soda.gd
@@ -1,13 +1,24 @@
 extends Enemy
 class_name Soda
 
+# Store the last direction to move in while player is dashing
+var last_direction: Vector2 = Vector2.ZERO
+
 
 func _physics_process(_delta: float) -> void:
 	if player == null:
 		return
 	
 	var direction = (player.global_position - global_position).normalized()
-	var move_velocity = direction * move_speed
+	var move_velocity
+	
+	if RunManager.player.is_dashing:
+		# Keep moving in the last known direction
+		move_velocity = last_direction * move_speed
+	else:
+		# Update direction towards player
+		last_direction = direction
+		move_velocity = last_direction * move_speed
 	
 	velocity = move_velocity + knockback_velocity
 	knockback_velocity = knockback_velocity.move_toward(Vector2.ZERO, 800 * _delta)
