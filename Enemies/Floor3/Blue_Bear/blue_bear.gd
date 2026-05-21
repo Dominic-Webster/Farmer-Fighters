d6b0862e55b85755343a73b67de1c86c31c51880
Dominic Webster <dominicbweiss@gmail.com>
2026-05-21 10:03:29 -0400

    blue bear

---
+++ Enemies/Floor3/Blue_Bear/blue_bear.gd
@@ -0,0 +1,52 @@
+extends Enemy
+class_name BlueBear
+
+@export var start_delay : Vector2 = Vector2(0.2, 0.4)
+var start_timer : float = randf_range(start_delay.x, start_delay.y)
+
+var switch_time : Vector2 = Vector2(0.4, 0.7)
+var switch_timer : float = 0.5
+var direction : Vector2 = Vector2.ZERO
+
+var rng = RandomNumberGenerator.new()
+
+
+func _physics_process(_delta: float) -> void:
+	if player == null:
+		return
+
+	if start_timer > 0:
+		start_timer -= _delta
+		return
+
+	switch_timer -= _delta
+	if switch_timer <= 0:
+		switch_timer = randf_range(switch_time.x, switch_time.y)
+		direction = Vector2(rng.randf_range(-1, 1), rng.randf_range(-1, 1)).normalized()
+		# Prevent zero direction
+		if direction == Vector2.ZERO:
+			direction = Vector2(1, 0)
+
+	var move_velocity = direction * move_speed
+
+	if RunManager.player.global_position.x < global_position.x:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
+
+	velocity = move_velocity + knockback_velocity
+	knockback_velocity = knockback_velocity.move_toward(Vector2.ZERO, 800 * _delta)
+
+	move_and_slide()
+
+
+func die():
+	if not is_dead:
+		died.emit()
+		is_dead = true
+		hurt_box.set_deferred("monitoring", false)
+		move_speed = 0
+		anim.stop()
+		anim.play("die")
+		await anim.animation_finished
+		queue_free()
