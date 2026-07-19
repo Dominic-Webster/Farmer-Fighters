0ee91573b9f19aa4d9e76a4908289524facd3b1c
Dominic Webster <dominicbweiss@gmail.com>
2026-07-18 23:41:04 -0400

    fix

--- 00_globals/RunManager.gd
+++ 00_globals/RunManager.gd
@@ -39,7 +39,7 @@ var test_item_6 = "res://Items/Common/Rhubarb/Rhubarb.tscn"
 
 func start_new_run(_player : Player):
 	
-	current_floor = 4
+	current_floor = 1
 	player_damaged_this_floor = false
 	
 	MapGenerationManager.create_new_map()
